7595f878162220c49651786b8ee8ef5494af4266
Josue Daniel <josuecanales0@gmail.com>
2019-09-17 01:06:41 -0600

    R2 Y R3 terminados

---
+++ dia_siguiente.py
@@ -0,0 +1,13 @@
+from fecha_es_valida import dias_de_mes as dias_de_mes
+
+def dia_siguiente(fecha):
+	dm = dias_de_mes(fecha)
+	anno, dia, mes = fecha
+	dia = dia + 1
+	if (dia > dm):
+		dia = 1
+		mes +=1
+	if (mes > 12):
+		mes = 1
+		anno +=1
+	return (anno,dia,mes)   
